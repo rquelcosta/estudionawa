07ca015f66501a6cdc9dd50011042264aec675d6
Raquel Costa <rquelcosta@gmail.com>
2017-07-12 16:02:15 -0300

    Algumas correções

--- back/app.js
+++ back/app.js
@@ -4,11 +4,11 @@ const express = require('express'),
   	helmet = require('helmet')
 
 app.set('view engine', 'ejs')
-app.set('views', './front')
+app.set('views', '../front')
 
 app.use(helmet())
 app.use(compression())
-app.use('/static', express.static('./front/assets/'))
+app.use('/static', express.static('../front/assets/'))
 
 app.use(function (req, res) {
   res.render('index');
@@ -18,4 +18,3 @@ app.listen(process.env.PORT || 3000, function () {
 	if(!process.env.PORT)
   		console.log('Example app listening on port 3000!');
 })
-
