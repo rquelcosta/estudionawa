63b79543dae1983ba02fdc96b37c6e697fd013ff
Raquel Costa <rquelcosta@gmail.com>
2017-07-13 13:06:09 -0300

    ta la também

--- back/app.js
+++ back/app.js
@@ -4,15 +4,15 @@ const express = require('express'),
   	helmet = require('helmet')
 
 app.set('view engine', 'ejs')
-app.set('views', '../front')
+app.set('views', './front')
 
 app.use(helmet())
 app.use(compression())
-app.use('/static', express.static('../front/assets/'))
+app.use('/static', express.static('./front/assets/'))
 
 app.use(function (req, res) {
   res.render('index');
-})
+}) 
 
 app.listen(process.env.PORT || 3000, function () {
 	if(!process.env.PORT)
